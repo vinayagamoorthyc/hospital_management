5ebf7f5a1a04084ec9264d7a848b5acdb8c3fc45
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2025-05-24 12:27:34 +0530

    implemented OOP in delete functionality

--- receptionist/delete.php
+++ receptionist/delete.php
@@ -3,13 +3,32 @@
 
     // db connection
     include "../db.php";
+    $db = new Database();
+    $conn = $db->getConnection();
+
+    // Appointment class
+    class Appointment{
+        private $conn;
+
+        public function __construct($conn){
+            $this->conn = $conn;
+        }
+
+        public function delete($_IdeleteId){
+            $stmt = $this->conn -> prepare("DELETE FROM appointment WHERE appointmentId = ?;");
+            $stmt->bindParam(1, $_IdeleteId);
+            $stmt->execute();
+        }
+    }
 
     if($_SERVER['HTTP_API_KEY'] == 'hospital123'){
+        $_IdeleteId = $_POST['deleteId'];
 
         // delete an appointment
         if($_POST['action'] == 'deleteAnAppointment'){
-            $_IdeleteId = $_POST['deleteId'];
-            $conn -> query("DELETE FROM appointment WHERE appointmentId = $_IdeleteId;");
+            $appointment = new Appointment($conn);
+
+            $appointment->delete($_IdeleteId);
             echo "deleted";
         }
     }
