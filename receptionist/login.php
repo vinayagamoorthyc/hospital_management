af1037dd1a9c23e71ab94e882160fb803f7d1db2
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2025-06-09 10:55:30 +0530

    added comment line for tailwind css

--- receptionist/login.php
+++ receptionist/login.php
@@ -36,6 +36,7 @@ if (isset($_POST['signin'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Tailwind CSS Script -->
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="../assets/css/recepLogin.css">
     <title>Login</title>
